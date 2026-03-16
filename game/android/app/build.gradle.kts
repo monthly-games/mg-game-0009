4c3a0fc7fed75cdc1c6a46a73eb2ef41e5aa3b76
jungsu-lunark <jungsu.kim@lunark.ai>
2026-03-16 17:05:03 +0900

    fix(android): standardize applicationId to com.monthlygames.game0009

--- game/android/app/build.gradle.kts
+++ game/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.mg_game_0009.game"
+    namespace = "com.monthlygames.game0009"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.mg_game_0009.game"
+        applicationId = "com.monthlygames.game0009"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
